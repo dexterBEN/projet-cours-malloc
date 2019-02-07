aea59b840cc57c58fe9aea91ae70170568569b85
dexterBEN <33292824+dexterBEN@users.noreply.github.com>
2019-02-07 14:45:44 +0100

    [feat]: test malloc implementation first-fit

--- my_malloc.c
+++ my_malloc.c
@@ -13,25 +13,69 @@
 ** return NULL: case sbrk failed
 */
 
-void *malloc(size_t size)
+void  *malloc(size_t size)
 {
-    void *p = sbrk(0);
-    void *request = sbrk(size);
+  void  *addr;// 
+  static int i = 0;
 
-    if (request == (void*) -1) {
-        return NULL;
-    } else {
-        assert(p == request);
-        return p;
-    }
+  //Dans le cas ou la taille rentrée vaut 0 on sort de l'éxécution
+  if(size == 0)
+  {
+    return (NULL);
+  }
+
+  size = (size - 1) / 4 * 4 + 4; //Alignement des pointer des block mémoires 
+  addr = find_block(size);
+
+  if(addr != NULL)
+  {
+    re_init_list();
+    return (addr);
+  }
+
+  //on agrandit la taille de la heap si jamais on ne trouve pas de block de taille suffisante
+  addr = sbrk(size); 
+
+  if (addr == (void *)-1)
+  {
+    printf("Error : sbrk() failed\n");
+    return (NULL);
+  }
+
+  if(i == 0)
+  {
+    g_list = NULL;
+  }
+
+  put_in_list(&g_list, size, addr);
+  ++i;
+
+  return (addr);
 }
 
-int main()
+void  *find_block(size_t size)
 {
-    void* new_brk = sbrk(0);
-    printf("La heap commence à l'adresse: %p\n", new_brk);
+  if(g_list == NULL)
+  {
+    return (NULL);
+  }
 
-    int *valueNull = my_malloc(0);
-    printf("Test value: %p \n", valueNull);
-    return (0);
+  if(g_list->is_used == UNUSED && size <= g_list->size)
+  {
+    g_list->is_used = USED;
+    return (g_list->addr);
+  }
+
+  while(g_list->head != 1)
+  {
+    if (g_list->is_used == UNUSED && size <= g_list->size)
+    {
+      g_list->is_used = USED;
+      return (g_list->addr);
+    }
+    g_list = g_list->next;
+  }
+
+  re_init_list();
+  return (NULL);
 }
