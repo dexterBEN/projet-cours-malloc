8ab91407bc8bc082d916cb090057bc8c9f4a5560
Slymaster <sylvain.rey.pro@gmail.com>
2019-02-07 14:11:30 +0100

    lib

---
+++ my_malloc.h
@@ -0,0 +1,18 @@
+/*
+** ETNA PROJECT, 07/02/2019 by rey_s, benoni_d
+** Malloc
+** File description:
+**      [...]
+*/
+
+#ifndef _MY_MALLOC_H_
+# define _MY_MALLOC_H_
+
+struct name {
+
+};
+
+void *malloc(size_t size);
+void free(void *ptr);
+
+#endif
