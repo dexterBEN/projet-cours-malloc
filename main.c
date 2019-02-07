5ab0c479ca15ce6a09932b5dc32858c1d09a053f
dexterBEN <33292824+dexterBEN@users.noreply.github.com>
2019-02-07 09:26:43 +0100

    [refactor]: respect standard etna coding style

--- main.c
+++ main.c
@@ -6,53 +6,64 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+//#include "free.c"
 
 //structure d'un block alloué/non-alloué
-struct block{
+struct block
+{
   size_t size; // taille du bloque que je soujai
   char free; //flag pour savoir si le block est free, 1->free , 0 -->not free 
   struct block *next;
 };
 
-/*void* my_malloc(size_t size) {
-  void *p = sbrk(0);
-  void *request = sbrk(size);
+void *my_malloc(size_t size) 
+{
+  void *p = sbrk(0); //The start address of the heap
+  void *request = sbrk(size); //change the heap size to allocate the suffisante space for user
 
-  if(request == (void*)-1) {
-    return NULL;// cas ou sbrk failed
+  if (request == (void*) -1) {
+    return NULL; //case sbrk failed.
   } else {
-    
-    printf("Voici le retour de malloc: %p\n", p);
+    assert(p == request); 
     return p;
   }
-}*/
-
-static unsigned char our_memory[1024*1024];
-static size_t next_index = 0;
-
-/*void *my_malloc(size_t sz)
-{
-  void *memory;
-
-  if(sizeof (our_memory) - next_index < sz){
-    return NULL;
-  }else{
-    memory = &our_memory[next_index];
-    next_index += sz;
-    
-    printf("Voici le retour de malloc: %p\n", memory);
-    return memory;
-  }
-
-}*/
+}
 
 int main() 
 {
   void* new_brk = sbrk(0);// retourne un pointeur contenant l'adresse du début de la heap
   printf("La heap commence à l'adresse: %p\n", new_brk);
 
-  printf("Voicic la taille d'un int: %i \n", sizeof(int));
-  //my_malloc(100);
-  //printf("heloo \n");
+  int *valueNull = my_malloc(0);
+  printf("Test value: %p \n", valueNull);
+
+  /*int *articles = NULL;
+  int nb_article = 0;
+  int i;
+  printf("How many articles: ");
+  scanf("%d", &nb_article);
+
+  articles =  my_malloc(sizeof(int)*nb_article);
+
+  if(articles == NULL)
+  {
+    exit(1);
+  }
+
+  for(i = 0; i < nb_article; i++)
+  {
+    printf("Item %d is in %d rayon \n", i + 1, i * 3);
+    articles[i] = i * 3; 
+  }
+
+  for(i = 0; i < nb_article; i++)
+  {
+    printf("[%d]", articles[i]);
+  }*/
+
+  //my_free(articles);
   return EXIT_SUCCESS;
 }
+
