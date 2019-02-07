76b6c474f2b8c31cecbbe3c2701d095e4d67878b
Slymaster <sylvain.rey.pro@gmail.com>
2019-02-07 14:14:17 +0100

    lib

--- my_malloc.h
+++ my_malloc.h
@@ -8,8 +8,14 @@
 #ifndef _MY_MALLOC_H_
 # define _MY_MALLOC_H_
 
-struct name {
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
 
+struct block {
+    size_t size;
+    char free;
+    struct block *next;
 };
 
 void *malloc(size_t size);
